667583291b93fab4ff0dadb144f50f373e3ffdf5
tpdinh10 <tpdinh10@gmail.com>
2026-03-19 13:02:49 -0700

    Add How To Play feature

--- start_menu.gd
+++ start_menu.gd
@@ -2,19 +2,19 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://WhereLightDies.tscn")
+	$click.play()
+	get_tree().change_scene_to_file("res://EndlessFog.tscn")
 
 func _on_options_pressed() -> void:
-	print("Options pressed")
+	$click.play()
+	print("options")
 
 func _on_exit_pressed() -> void:
+	$click.play()
 	get_tree().quit()
+
+
+func _on_how_to_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://HowToPlay.tscn")
