d53ec22cca5d9ff59ccddd93d68207c1033fb688
Kevin <33043253+KevinJAlvarez@users.noreply.github.com>
2026-04-03 18:11:18 -0700

    Added hard map along with pause menu fixes

--- pause_menu.gd
+++ pause_menu.gd
@@ -3,6 +3,13 @@ extends CanvasLayer
 func _ready() -> void:
 	visible = false
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if get_tree().paused:
+			resume_game()
+		else:
+			pause_game()
 
 func pause_game() -> void:
 	get_tree().paused = true
